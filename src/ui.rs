fa1e256fa9bcae0a783e943a86c6e2321ae7fb00
Calastrophe <zediant@gmail.com>
2025-02-11 23:00:57 -0600

    split right panel vertically

--- src/ui.rs
+++ src/ui.rs
@@ -4,8 +4,8 @@ mod editor;
 mod top;
 mod variable_viewer;
 
-use egui::{TopBottomPanel, Ui};
 use crate::debugger::Debugger;
+use egui::Ui;
 
 use bytecode_viewer::BytecodeViewer;
 use cpu::CpuState;
@@ -23,11 +23,10 @@ macro_rules! impl_component_name {
     };
 }
 
-pub trait Component { 
+pub trait Component {
     fn name(&self) -> &'static str;
     fn show(&mut self, ui: &mut Ui);
 }
-
 
 #[derive(Default)]
 pub struct Interface {
@@ -38,7 +37,6 @@ pub struct Interface {
     pub editor: Editor,
     pub top: Top,
     pub variable_viewer: VariableViewer,
-
 }
 
 impl Interface {
@@ -51,39 +49,39 @@ impl Interface {
 
 impl eframe::App for Interface {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| { 
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             self.top.show(ui);
         });
 
+        let available_width = ctx.available_rect().width();
+        let min_width = available_width / 6.0;
+
         egui::SidePanel::left("left_panel")
             .resizable(false)
-            .min_width(300.0) // todo; make this min width atleast like 1/3 of the screen size 
+            .min_width(min_width)
             .show(ctx, |ui| {
                 self.bytecode_viewer.show(ui);
-        });
+            });
 
-        
         egui::CentralPanel::default().show(ctx, |ui| {
             self.editor.show(ui);
         });
 
-
-        egui::SidePanel::right("right")
+        egui::SidePanel::right("right_panel")
             .resizable(false)
-            .min_width(300.0) // todo; make this min width atleast like 1/3 of the screen size 
-            .show(ctx, |ui| {  
+            .min_width(min_width)
+            .show(ctx, |ui| {
+                let (top, bottom) = ui
+                    .available_rect_before_wrap()
+                    .split_top_bottom_at_fraction(0.5);
 
-                egui::Grid::new("right_grid").num_columns(1).show(ui, |ui| { 
+                ui.allocate_new_ui(egui::UiBuilder::new().max_rect(top), |ui| {
                     self.cpu_state.show(ui);
-                    ui.end_row();
-                    ui.separator();
-                    ui.end_row();
-                    self.bytecode_viewer.show(ui);
                 });
-        });
 
-
-
-
+                ui.allocate_new_ui(egui::UiBuilder::new().max_rect(bottom), |ui| {
+                    self.variable_viewer.show(ui);
+                });
+            });
     }
 }
