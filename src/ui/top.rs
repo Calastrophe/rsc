33d20a37508628657c875ab0c967f2e224de3a7a
vablings <47798035+vablings@users.noreply.github.com>
2025-02-12 21:39:01 -0600

    changed hooks

--- src/ui/top.rs
+++ src/ui/top.rs
@@ -12,35 +12,25 @@ impl Top {
         &mut self,
         ui: &mut egui::Ui,
         debugger: &mut Option<Debugger>,
-        
+
         assembler: &mut Option<Assembler>,
         code: &str,
     ) {
-
-
-        ui.vertical_centered(|ui| { 
-            ui.horizontal(|ui| { 
-     
-            if let Some(debugger) =  debugger {
-                ui.button(""); // pause
-                ui.button("▶️"); // 
-                ui.add(
-                    egui::Slider::new::<u32>(&mut debugger.instructions_per_second , 1..=1000)
-                    .text("IPS")
-
-                );
-            } else { 
-                if ui.button("Assemble").clicked() { 
-                    assembler.replace(Assembler::parse(code.to_string()));
+        ui.vertical_centered(|ui| {
+            ui.horizontal(|ui| {
+                if let Some(debugger) = debugger {
+                    ui.button(""); // pause
+                    ui.button("▶️"); //
+                    ui.add(
+                        egui::Slider::new::<u32>(&mut debugger.instructions_per_second, 1..=1000)
+                            .text("IPS"),
+                    );
+                } else {
+                    if ui.button("Assemble").clicked() {
+                        assembler.replace(Assembler::parse(code.to_string()));
+                    }
                 }
-
-            }
-
             });
-
-
-
-
         });
     }
 }
